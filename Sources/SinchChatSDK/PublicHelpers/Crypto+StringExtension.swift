9630767fa5fe53acc96df6e3177ed2247331365f
Piotr Krzesaj <krzesaj.piotr.5@gmail.com>
2023-04-19 09:47:40 +0200

    fix: expose hmac helper function

---
+++ Sources/SinchChatSDK/PublicHelpers/Crypto+StringExtension.swift
@@ -0,0 +1,30 @@
+import Foundation
+import CommonCrypto
+
+public extension String {
+
+    func hmac(algorithm: CryptoAlgorithm, key: String) -> String {
+        let str = self.cString(using: String.Encoding.utf8)
+        let strLen = Int(self.lengthOfBytes(using: String.Encoding.utf8))
+        let digestLen = algorithm.digestLength
+        let result = UnsafeMutablePointer<CUnsignedChar>.allocate(capacity: digestLen)
+        let keyStr = key.cString(using: String.Encoding.utf8)
+        let keyLen = Int(key.lengthOfBytes(using: String.Encoding.utf8))
+
+        CCHmac(algorithm.HMACAlgorithm, keyStr!, keyLen, str!, strLen, result)
+
+        let digest = stringFromResult(result: result, length: digestLen)
+
+        result.deallocate()
+
+        return digest
+    }
+
+    private func stringFromResult(result: UnsafeMutablePointer<CUnsignedChar>, length: Int) -> String {
+        let hash = NSMutableString()
+        for counter in 0..<length {
+            hash.appendFormat("%02x", result[counter])
+        }
+        return String(hash).lowercased()
+    }
+}
